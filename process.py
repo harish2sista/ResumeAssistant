727c6e422ea7214f60b8469143f68f09ee5e4d54
Harish Sista <harish2sista@gmail.com>
2024-09-03 22:08:35 -0400

    updated data paths and removed api_key

--- process.py
+++ process.py
@@ -19,7 +19,11 @@ def get_img(pdf_path):
 
 # get_img('Harish_Resume_PhD.pdf')
 
-client = OpenAI(api_key="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")
+with open('gpt_key.json', 'r') as file:
+      data = json.load(file)
+      api_key = data["api_key"]
+
+client = OpenAI(api_key="api_key")
 
 
 sys_prompt = "you are a resume parser. Given the image of the resume, parse every section and provide the output in a json format."
@@ -28,4 +32,4 @@ user_prompt="can you parse all sections of this resume?"
 
 image_worker = ImageWorker(client=client, model_id="gpt-4o-mini", sys_prompt=sys_prompt, user_prompt=user_prompt)
 # print(image_worker.get_output('page0.jpg'))
-print(image_worker.get_output('Resumes_Datasets/Bing_images/Accountant_resumes/Image_43.jpg'))
+print(image_worker.get_output('Resumes Datasets/Bing images/Accountant_resumes/Image_43.jpg'))
