eea691c9fc03691c4a87a26672eb32a45096e3a9
Harish Sista <harish2sista@gmail.com>
2024-09-19 21:24:30 -0400

    bug fix

--- process.py
+++ process.py
@@ -14,11 +14,9 @@ import json
 def get_img(pdf_path, img_path):
       # Method for converting pdf resumes to jpg images. 
       images = convert_from_path(pdf_path)
-      # print(type(images))
       for i in range(len(images)):
-        
             # Save pages as images in the pdf
-          images[i].save(img_path + str(i) +'.jpg', 'JPEG')
+            images[i].save(img_path + str(i) +'.jpg', 'JPEG')
 
 
 
@@ -38,7 +36,7 @@ sys_prompt = "You are a resume parser. Given the image of the resume, parse ever
 user_prompt="Can you parse all sections of this resume?"
 image_worker = ImageWorker(client=client, model_id="gpt-4o-mini", sys_prompt=sys_prompt, user_prompt=user_prompt)
 parsed_resume = image_worker.get_output('Harish_Resume0.jpg')
-print(f'parsed_resume: \n{parsed_resume}')
+print(f'parsed_resume: \n{parsed_resume}', flush=True)
 
 #Old cover-letter
 old_cl = "Hello Hiring Team, \nI am pursuing my Ph.D. in the Computer Engineering department at Stevens Institute of Technology; my research is focused on Natural Language Processing. I work on applications related to the areas of Misinformation Research and Investigative Journalism using Evidence Extraction, Fact Verification and Key-Phrase analysis; I research and work with Generative Models, Deep Learning models, Attention Networks, and Large Language Models for Extraction and Verification Tasks. My Dissertation Project is based on Fact-Verification and Evidence-Extraction using an Explainable Prompt-Engineering Model I proposed. \nEven though I chose the use cases of NLP as the focus of my research, my work is more comprehensive than NLP-based models;  I have also created a low-cost Derivative-Free Optimization Method for parameter optimization in ML; I am working on obtaining a Provisional Patent for this research. I am immensely passionate about learning and analyzing the underlying Mathematical Concepts of Machine Learning Models; I like educating myself on all the specifics of any problem statement, following the motivation of the research, chronological evolution of the Problem-Solution framework, and feature analysis from historical to the SOTA models. \nI am currently seeking a job opportunity to hone my skills and work with a proficient team for an inspirational cause where AI can be efficacious. \nRegards, \nHarish Sista, Ph.D. candidate, \nComputer Engineering Department, \nStevens Institute of Technology."
@@ -50,12 +48,12 @@ record = Record(client=client, resume=parsed_resume, cl=old_cl)
 #Job description
 with open('job_description2.txt', 'r') as f:
     job_description2 = f.read()
-new_cl = record.generate_cl(job_description2)
-print(f'New Cover Letter: \n{new_cl}')
+new_cl = record.generate_cl(job_description=job_description2)
+print(f'New Cover Letter: \n{new_cl}', flush=True)
 
 #Resume Screening
-key_questions = record.screen_resume_CL(job_description2)
-print(f'Screening Question on missing candidate information: \n{key_questions}')
+key_questions = record.screen_resume_CL(job_description=job_description2)
+print(f'Screening Question on missing candidate information: \n{key_questions}', flush=True)
 
 
 #Clreate Additional Knowledge record
@@ -90,7 +88,7 @@ key_knowledge = {'5+ years of experience in working with AI/LLMs':
                   'question': 'Can you provide examples from your resume that demonstrate your experience collaborating with cross-functional teams to integrate AI solutions?',
                  'answer': 'In my previous job at \'Fresh Digital Group,\' I have collaborated with various teams like content writers and UI designers to brainstorm on building structure for chat applications and designing UI for mobile applications.'}}
 add_knowledge.add_knowledge(key_knowledge)
-print(f'Additional Knowledge to be saved: \n{add_knowledge.get_knowledge()}')
+print(f'Additional Knowledge to be saved: \n{add_knowledge.get_knowledge()}', flush=True)
 
 
 #Record QA_Knowledge instance to candidate record
@@ -98,13 +96,13 @@ record.add_knowledge.update(add_knowledge.get_knowledge())
 
 
 #Candidate Record in JSON format
-print(f'Candidate Record in JSON format: \n{record.get_record()}')
+print(f'Candidate Record in JSON format: \n{record.get_record()}', flush=True)
 
 
 #Generate new cover letter based on job_description2
 with open('job_description2.txt', 'r') as f:
     job_description2 = f.read()
-new_cl = record.generate_cl(job_description2)
-print(f'New Cover Letter after adding add_knowledge: \n{new_cl}')
+new_cl = record.generate_cl(job_description=job_description2)
+print(f'New Cover Letter after adding add_knowledge: \n{new_cl}', flush=True)
 
 
